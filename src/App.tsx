ea888e4f47f245c46cbe6e92929b1be76f6f7801
pmw0310 <mw.park@retigrid.com>
2022-08-03 11:12:37 +0900

    테스트맵 작동

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,43 @@
 import React from 'react';
 import './App.css';
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import icon from "leaflet/dist/images/marker-icon.png";
+import { MapContainer, Marker, Popup, ImageOverlay } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import { Icon } from 'leaflet';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import { LatLngBoundsLiteral, LatLngTuple, CRS } from 'leaflet';
 
-const position: [number, number] = [51.505, -0.09]
+const position: LatLngTuple = [1024, 1024];
+const bounds: LatLngBoundsLiteral = [
+   [1, 1],
+   [1024, 1024],
+];
+
+const MarkerIcon = new Icon({
+   iconUrl: markerIcon,
+   iconSize: [25, 41],
+   iconAnchor: [12, 41],
+});
 
 const App: React.FC = () => {
-  return (
-    <div style={{height: '100vh'}}>
-      <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{height: '100%'}}>
-         <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-         />
-         <Marker position={position}>
-            <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-         </Marker>
-      </MapContainer>
-    </div>
-  );
-}
+   return (
+      <div style={{ height: '100vh' }}>
+         <MapContainer
+            maxZoom={2}
+            maxBounds={bounds}
+            // scrollWheelZoom={false}
+            crs={CRS.Simple}
+            bounds={bounds}
+            style={{ height: '100%' }}
+         >
+            <ImageOverlay url="/maps/test_map.png" bounds={bounds} />
+            <Marker position={position} icon={MarkerIcon}>
+               <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+               </Popup>
+            </Marker>
+         </MapContainer>
+      </div>
+   );
+};
 
 export default App;
