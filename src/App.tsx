6746a1923a5572757ca256a46829061024c8f0f3
pmw0310 <mw.park@retigrid.com>
2022-08-04 11:55:27 +0900

    라우터 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Map from './components/Map';
 
 const App: React.FC = () => {
    return (
-      <div className="App">
-         <BrowserRouter>
-            <Routes>
-               <Route path="/map/:mapName" element={<Map />}></Route>
-               <Route path="*" element={<div>404</div>}></Route>
-            </Routes>
-         </BrowserRouter>
-      </div>
+      <>
+         <Routes>
+            <Route path="/map/:mapName" element={<Map />}></Route>
+            <Route path="*" element={<div>404</div>}></Route>
+         </Routes>
+      </>
    );
 };
 
